3347fd12b73b97a2e5718d5995790be7bd0a3546
Chaemin07 <gkc05075@gmail.com>
2025-02-27 20:28:54 +0900

    정수입력 오류 해결

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -10,32 +10,34 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
         // 크기가 2인 long타입 배열 선언
         long[] calcParams = new long[2];
-        // 중간 계산 결과 저장 부족하면 늘릴 생각
+        // 중간 계산 결과, 저장 부족하면 늘릴 생각
         String[] interimCalcResult = new String[10];
-        String operator = "";
         // 계산 결과
         long result = 0;
         int cnt=0;
+        String token = "";
+        String operator = "";
 
-        String flag = "";
-        while (!(flag.equals("q") || flag.equals("Q") || flag.equals("exit"))){
+
+        while (!(token.equals("q") || token.equals("Q") || token.equals("exit"))){
             System.out.println("==============================================");
             System.out.println("1. 계산");
             System.out.println("2. 계산 기록보기");
             System.out.println("종료: \"exit\", \"q\",\"Q\" 입력하기");
 
-            switch (flag = sc.next()){
+            // token값을 이용한 switch문
+            switch (token = sc.next()){
                 case "1":       // 계산
                     System.out.println("1. 계산을 선택하셨습니다!");
                     // 두 개의 양의 정수 입력
                     for (int i = 0; i < 2; i++) {
-                        System.out.println("계산할 "+i+"번째양의 정수를 입력해주세요");
+                        System.out.println("계산할 "+i+"번째 양의 정수를 입력해주세요");
                         if (sc.hasNextLong()) {
                             calcParams[i] = sc.nextLong();
                             System.out.println("현재 입력된 값은 " + calcParams[i]);
                         } else {
                             i-=1;
-                            if(i<0){
+                            if(i<-1){
                                 i=0;
                             }
                             sc.next();
@@ -44,7 +46,7 @@ public class Calculator {
                     }
                     System.out.println("사용할 연산자를 입력해주세요!");
                     // TODO swtich문 작성 #########################
-                    switch (operator = sc.next()) {
+                    switch (operator = sc.nextLine()) {
                         case "+": // 더하기
                             result = calcParams[0] + calcParams[1];
                             break;
@@ -63,7 +65,7 @@ public class Calculator {
                         default:
                             System.out.println("다시 입력해주세요.");
                             sc.next();
-                            flag = "false";
+
                     }
                     // TODO 입력 오류(유효하지 않는 값) 수정 #########################
 
@@ -79,9 +81,8 @@ public class Calculator {
                             " "+String.valueOf(calcParams[1])+" =  "+String.valueOf(result);
 
                     System.out.println("==============================================");
-
-
                     break;
+
                 case "2":       // 계산 기록
                     System.out.println("2. 계산 기록보기를 선택하셨습니다!");
                     for(int i=0; i<cnt; i++){
@@ -89,13 +90,13 @@ public class Calculator {
                     }
                     break;
                 case "exit":    // 종료
-                    break;
                 case "q":       // 종료
-                    break;
                 case "Q":       // 종료
+                    System.out.println("종료를 선택하셨습니다!");
                     break;
                 default:
                     System.out.println("다시 입력해주세요");
+                    break;
             }
 
         }
