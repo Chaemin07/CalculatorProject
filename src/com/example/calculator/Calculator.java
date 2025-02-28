893aef381b29e8dc005b21aad8bea690c6b377b0
Chaemin07 <gkc05075@gmail.com>
2025-02-28 10:27:52 +0900

    ì입력 오류 수정

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -10,20 +10,22 @@ public class Calculator {
         Scanner sc = new Scanner(System.in);
         // 크기가 2인 long타입 배열 선언
         long[] calcParams = new long[2];
-        // 중간 계산 결과, 저장 부족하면 늘릴 생각
+        // 중간 계산 결과, 일단 10정도만
         String[] interimCalcResult = new String[10];
         // 계산 결과
         long result = 0;
+        long calcParam = 0;
         int cnt=0;
+        boolean switchFlag = true;
         String token = "";
         String operator = "";
 
 
         while (!(token.equals("q") || token.equals("Q") || token.equals("exit"))){
             System.out.println("==============================================");
-            System.out.println("1. 계산");
+            System.out.println("1. 계산하기");
             System.out.println("2. 계산 기록보기");
-            System.out.println("종료: \"exit\", \"q\",\"Q\" 입력하기");
+            System.out.println("3. 종료: \"exit\", \"q\",\"Q\" 입력하기");
 
             // token값을 이용한 switch문
             switch (token = sc.next()){
@@ -31,64 +33,81 @@ public class Calculator {
                     System.out.println("1. 계산을 선택하셨습니다!");
                     // 두 개의 양의 정수 입력
                     for (int i = 0; i < 2; i++) {
-                        System.out.println("계산할 "+i+"번째 양의 정수를 입력해주세요");
-                        if (sc.hasNextLong()) {
-                            calcParams[i] = sc.nextLong();
-                            System.out.println("현재 입력된 값은 " + calcParams[i]);
-                        } else {
+                        System.out.println("계산할 "+(i+1)+"번째 양의 정수를 입력해주세요");
+                        // 추후 String으로 입력받아 long,double로 수정
+                        // 정수인지 판별
+                        if (!sc.hasNextLong()) {
                             i-=1;
                             if(i<-1){
                                 i=0;
                             }
-                            sc.next();
-                            System.out.println("잘못된 값이 입력되었습니다! 현재 i = "+i);
+                            System.out.println("잘못된 값이 입력되었습니다!\n다시 입력해주세요!");
+                            // long -> String 버퍼 비우기
+                            sc.nextLine();
+                        }else{
+                            // 양수인지 판별
+                            calcParam = sc.nextLong();
+                            System.out.println("현재 입력된 값은 " + calcParam);
+                            if(calcParam>=0){
+                                calcParams[i] = calcParam;
+                            }else{
+                                i-=1;
+                                if(i<-1){
+                                    i=0;
+                                }
+                                System.out.println("음수가 입력되었습니다!\n다시 입력해주세요!");
+                            }
                         }
+
                     }
                     System.out.println("사용할 연산자를 입력해주세요!");
-                    // TODO operator입력 오류 버퍼 문제? #########################
-                    sc.next();
-                    // TODO swtich문 작성 #########################
-                    switch (operator = sc.nextLine()) {
-                        case "+": // 더하기
-                            result = calcParams[0] + calcParams[1];
-                            break;
-                        case "-": // 빼기
-                            result = calcParams[0] - calcParams[1];
-                            break;
-                        case "*": // 곱하기
-                            result = calcParams[0] * calcParams[1];
-                            break;
-                        case "/": // 나누기 - 몫
-                            result = calcParams[0] / calcParams[1];
-                            break;
-                        case "%": // 나누기 - 나머지
-                            result = calcParams[0] % calcParams[1];
-                            break;
-                        default:
-                            System.out.println("다시 입력해주세요.");
-                            sc.next();
+                    // long -> String 버퍼 비우기
+                    sc.nextLine();
 
+                    // switchFlag 초기화
+                    switchFlag = true;
+                    while(switchFlag){
+                        switch (operator=inputOperator()) {
+                            case "+": // 더하기
+                                result = calcParams[0] + calcParams[1];
+                                switchFlag = false;
+                                break;
+                            case "-": // 빼기
+                                result = calcParams[0] - calcParams[1];
+                                switchFlag = false;
+                                break;
+                            case "*": // 곱하기
+                                result = calcParams[0] * calcParams[1];
+                                switchFlag = false;
+                                break;
+                            case "/": // 나누기(몫)
+                                result = calcParams[0] / calcParams[1];
+                                switchFlag = false;
+                                break;
+                            case "%": // 나누기(나머지)
+                                result = calcParams[0] % calcParams[1];
+                                switchFlag = false;
+                                break;
+                            default:
+                                System.out.println("다시 입력해주세요.");
+                        }
                     }
-                    // TODO 입력 오류(유효하지 않는 값) 수정 #########################
 
                     // 연산자 입력
-                    String calcOperator = sc.next();
                     for (int i = 0; i < 2; i++) {
                         System.out.println((i+1)+"번째 입력된 값: "+calcParams[i]);
                     }
-                    System.out.println("입력된 연산자: "+calcOperator);
+                    System.out.println("입력된 연산자: "+operator);
 
-                    System.out.println("= " + result);
+                    System.out.println("결과 :  " + result);
                     interimCalcResult[cnt++] = String.valueOf(calcParams[0])+" "+operator +
                             " "+String.valueOf(calcParams[1])+" =  "+String.valueOf(result);
-
-                    System.out.println("==============================================");
                     break;
 
                 case "2":       // 계산 기록
                     System.out.println("2. 계산 기록보기를 선택하셨습니다!");
                     for(int i=0; i<cnt; i++){
-                        System.out.println(i+"번째 결과 : "+interimCalcResult[i]);
+                        System.out.println((i+1)+"번째 결과 : "+interimCalcResult[i]);
                     }
                     break;
                 case "exit":    // 종료
@@ -103,10 +122,16 @@ public class Calculator {
 
         }
     }
-
-    public static void clear() {
-        for (int i = 0; i < 17; i++) {
-            System.out.println();
-        }
+    // switch문 연산자 입력
+    public static String inputOperator() {
+        Scanner sc = new Scanner(System.in);
+        String operator = "";
+        operator = sc.next(); // 엔터까지 전부 처리?
+        System.out.println("현재 입력된 operator는 " + operator);
+        // 현재 버퍼에는?
+        String currentBuffer = sc.nextLine();
+        System.out.println("현재 버퍼 내용 : " + currentBuffer);
+        System.out.println("현재 버퍼 크기 : " + currentBuffer.length());
+        return operator;
     }
 }
